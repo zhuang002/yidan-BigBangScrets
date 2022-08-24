7772a8df14dd29b4703cf1827b7ded88d501414c
zhuang002 <zhuang001@gmail.com>
2022-08-24 17:49:38 -0400

    Version 1.0 -- add some comments

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+# This is the decoding method to decode ONE character
 def decode(ch, shift):
     shift %= 26
+    # wrap the shift
     shift = 26 - shift
-    offset = (ord(ch)-ord('A')+shift) % 26
-    return chr(ord('A')+offset)
+    # convert shift left to shift right
+    offset = (ord(ch) - ord('A') + shift) % 26
+    # wrap the offset
+    return chr(ord('A') + offset)
+    # add offset back to ascii.
+
 
 k = int(input())
 encoded = input()
 decoded = ""
-idx=1
+idx = 1
 
 for c in encoded:
-    decoded += decode(c, 3*idx+k)
+    decoded += decode(c, 3 * idx + k)
     idx += 1
 print(decoded)
